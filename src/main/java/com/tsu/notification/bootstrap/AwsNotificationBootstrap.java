7d9d7e9e8d53e679116a638b79e54142032ff59a
shawn <shawn1807@gmail.com>
2026-02-27 16:14:59 +0800

    fix compile

---
+++ src/main/java/com/tsu/notification/bootstrap/AwsNotificationBootstrap.java
@@ -0,0 +1,12 @@
+package com.tsu.notification.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsNotificationBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsNotificationBootstrap.class);
+    }
+}
